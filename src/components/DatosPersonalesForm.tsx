c7b00de4ba849cc10c9b3e66c54ed1991556be74
Victor Pinedo <victorpinedomartinez@gmail.com>
2025-02-10 12:28:44 -0500

    refactor datos personales to be concise

--- src/components/DatosPersonalesForm.tsx
+++ src/components/DatosPersonalesForm.tsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import { Snackbar, Alert } from '@mui/material';
-import { AutoGridRow } from '@components/AutoGridRow';
-import { FormButtons } from '@components/FormButtons';
 import { Typography, Paper } from '@mui/material';
+import { AutoGridRow } from '@components/AutoGridRow';
 import { RenderFormFields } from '@components/RenderFormFields';
 import { useDatosPersonalesForm } from '@hooks/useDatosPersonalesForm';
 import { datosPersonalesCampos } from '@utils/datosPersonalesFormConfig';
 
 export const DatosPersonalesForm: React.FC = () => {
-  const {
-    watch,
-    errors,
-    setValue,
-    register,
-    onSubmit,
-    handleSubmit,
-    openSnackbar,
-    handleSnackbarClose
-  } = useDatosPersonalesForm();
+  const { watch, errors, setValue, register } = useDatosPersonalesForm();
   
   return (
     <Paper elevation={3} sx={{ padding: 3, marginBottom: 3 }}>
@@ -25,35 +14,20 @@ export const DatosPersonalesForm: React.FC = () => {
         Datos Personales
       </Typography>
 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        {datosPersonalesCampos.map((fila, rowIndex) => (
-          <AutoGridRow key={rowIndex} rowSpacing={2}>
-            {fila.map((campo) => (
-              <RenderFormFields
-                key={campo.name}
-                campo={campo}
-                register={register}
-                watch={watch}
-                setValue={setValue}
-                errors={errors}
-              />
-            ))}
-          </AutoGridRow>
-        ))}
-
-        <FormButtons handleSubmit={handleSubmit} onSubmit={onSubmit} />
-      </form>
-
-      <Snackbar
-        open={openSnackbar}
-        autoHideDuration={3000}
-        onClose={handleSnackbarClose}
-        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-      >
-        <Alert onClose={handleSnackbarClose} severity='success'>
-          Datos guardados correctamente
-        </Alert>
-      </Snackbar>
+      {datosPersonalesCampos.map((fila, rowIndex) => (
+        <AutoGridRow key={rowIndex} rowSpacing={2}>
+          {fila.map((campo) => (
+            <RenderFormFields
+              key={campo.name}
+              campo={campo}
+              register={register}
+              watch={watch}
+              setValue={setValue}
+              errors={errors}
+            />
+          ))}
+        </AutoGridRow>
+      ))}
     </Paper>
   );
 };
--- src/components/HojaDeVidaForm.tsx
+++ src/components/HojaDeVidaForm.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
+import { Snackbar, Alert } from '@mui/material';
+import { FormButtons } from '@components/FormButtons';
 import { DatosPersonalesForm } from '@components/DatosPersonalesForm';
+import { useDatosPersonalesForm } from '@hooks/useDatosPersonalesForm';
 
 export const HojaDeVidaForm: React.FC = () => {
-  return <DatosPersonalesForm />;
+  const { onSubmit, handleSubmit, openSnackbar, handleSnackbarClose } = useDatosPersonalesForm();
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <DatosPersonalesForm />
+      <FormButtons handleSubmit={handleSubmit} onSubmit={onSubmit} />
+
+      <Snackbar
+        open={openSnackbar}
+        autoHideDuration={3000}
+        onClose={handleSnackbarClose}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+      >
+        <Alert onClose={handleSnackbarClose} severity='success'>
+          Datos guardados correctamente
+        </Alert>
+      </Snackbar>
+    </form>
+  );
 };
