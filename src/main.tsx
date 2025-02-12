847ba55781dec5f20cf1227fcee6f35a6450f2be
Victor Pinedo <victorpinedomartinez@gmail.com>
2025-02-12 13:54:39 -0500

    add and configura tanstack react query

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { App } from './App'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { configureDayjs } from '@utils/configureDayjs';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 configureDayjs();
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
