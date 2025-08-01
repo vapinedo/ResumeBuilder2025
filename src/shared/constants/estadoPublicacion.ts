bbd0a3116fd4a73e5dbf49275009dd218d8036af
Victor Pinedo <victor.pinedo@maritz.com>
2025-08-01 16:14:18 -0500

    build success

---
+++ src/shared/constants/estadoPublicacion.ts
@@ -0,0 +1,5 @@
+export enum Estado {
+  Activo = 'Activo',
+  Inactivo = 'Inactivo',
+  Pendiente = 'Pendiente',
+}
--- src/shared/pages/HomePage.tsx
+++ src/shared/pages/HomePage.tsx
@@ -1,5 +1,6 @@
+// src/shared/pages/HomePage.tsx
 import React from 'react';
-import { ResumeForm } from '@feature/resume/components/ResumeForm';
+import ResumeForm from '@feature/resume/components/ResumeForm';
 
 export const HomePage: React.FC = () => {
   return <ResumeForm />;
