d38b14e0c60157094d030e614f154bcd3cdb91f9
Victor Pinedo <victorpinedomartinez@gmail.com>
2025-08-01 16:15:31 -0500

    Prepare For Build and Deploy (#9)

--- src/core/repositories/IResumeRepository.ts
+++ src/core/repositories/IResumeRepository.ts
@@ -1,10 +1,11 @@
+// src/core/repositories/IResumeRepository.ts
 import { Resume } from '@core/models';
 
 export interface IResumeRepository {
   listar(): Promise<Resume[]>;
   obtenerPorId(id: string): Promise<Resume | null>;
-  crear(persona: Resume, images?: FileList | null): Promise<void>;
-  actualizar(persona: Resume, images?: FileList | null): Promise<void>;
+  crear(resume: Resume, images?: FileList | null): Promise<void>;
+  actualizar(resume: Resume & { id: string }, images?: FileList | null): Promise<void>;
   eliminar(id: string): Promise<void>;
   contar(): Promise<number>;
 }
--- src/core/repositories/index.ts
+++ src/core/repositories/index.ts
@@ -1,1 +1,2 @@
 export * from './IPersonaRepository';
+export * from './IResumeRepository';
